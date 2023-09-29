c7d92a2d2b5f347305fa39eeb102357639dd1cd7
Denis Costello <G00396309@atu.ie>
2023-09-29 18:42:09 +0100

    First commit of calculator homework

---
+++ src/main/java/ie/atu/calculatorhomework/CalculatorHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.calculatorhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorHomeworkApplication.class, args);
+    }
+
+}
