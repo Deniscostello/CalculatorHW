35cb7e562ce64c1b4c5fb339e457dce1e64cae3c
Denis Costello <G00396309@atu.ie>
2023-10-02 17:54:11 +0100

    Added new calculator obj class

--- src/main/java/ie/atu/calculatorhomework/CalculatorController.java
+++ src/main/java/ie/atu/calculatorhomework/CalculatorController.java
@@ -30,36 +30,8 @@ public class CalculatorController {
                 throw new IllegalArgumentException("Error: Invalid operation");
 
         }
-        CalculatorObj Calobject = new CalculatorObj(result, operation);
-        return Calobject;
-    }
-
-    public class CalculatorObj{
-
-        public int getResult() {
-            return result;
-        }
-
-        public void setResult(int result) {
-            this.result = result;
-        }
-
-        private int result;
-
-        public String getOperation() {
-            return operation;
-        }
-
-        public void setOperation(String operation) {
-            this.operation = operation;
-        }
-
-        private String operation;
-        public CalculatorObj(int result, String operation){
-            this.result=result;
-            this.operation=operation;
-        }
-
+        CalculatorObj object = new CalculatorObj(result, operation);
+        return object;
     }
 }
 
